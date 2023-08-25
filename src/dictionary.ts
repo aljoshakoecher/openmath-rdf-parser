7ee4411f2d4288ae8f4d5429324b5b97f3f33b9b
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2023-08-25 10:15:43 +0200

    add translations for relations

--- src/dictionary.ts
+++ src/dictionary.ts
@@ -1,5 +1,13 @@
 export class OperatorDictionary {
 	private static mathJsToOpenMathMapping = {
+		// Relations
+		"=": "http://www.openmath.org/cd/relation1#eq",
+		"<": "http://www.openmath.org/cd/relation1#lt", 
+		">": "http://www.openmath.org/cd/relation1#gt", 
+		"!=": "http://www.openmath.org/cd/relation1#neq",
+		"<=": "http://www.openmath.org/cd/relation1#leq",
+		">=": "http://www.openmath.org/cd/relation1#geq", 
+		// Arithmetic operators
 		"+": "http://www.openmath.org/cd/arith1#plus",
 		"-": "http://www.openmath.org/cd/arith1#minus",
 		"*": "http://www.openmath.org/cd/arith1#times",
@@ -7,12 +15,21 @@ export class OperatorDictionary {
 		"sqrt": "http://www.openmath.org/cd/arith1#root",
 		"pow": "http://www.openmath.org/cd/arith1#power",
 		"abs": "http://www.openmath.org/cd/arith1#abs",
+		// Transcendental functions
 		"sin": "http://www.openmath.org/cd/transc1#sin",
 		"cos": "http://www.openmath.org/cd/transc1#cos",
 		"tan": "http://www.openmath.org/cd/transc1#tan",
 	};
 
 	private static openMathToMathJsMapping = {
+		// Relations
+		"http://www.openmath.org/cd/relation1#eq": "=",
+		"http://www.openmath.org/cd/relation1#lt": "<",
+		"http://www.openmath.org/cd/relation1#gt": ">",
+		"http://www.openmath.org/cd/relation1#neq": "!=",
+		"http://www.openmath.org/cd/relation1#leq": "<=",
+		"http://www.openmath.org/cd/relation1#geq": ">=",
+		// Arithmetic operators
 		"http://www.openmath.org/cd/arith1#plus": "+",
 		"http://www.openmath.org/cd/arith1#minus": "-",
 		"http://www.openmath.org/cd/arith1#times": "*",
@@ -20,6 +37,7 @@ export class OperatorDictionary {
 		"http://www.openmath.org/cd/arith1#root": "sqrt",
 		"http://www.openmath.org/cd/arith1#power": "pow",
 		"http://www.openmath.org/cd/arith1#abs": "abs",
+		// Transcendental functions
 		"http://www.openmath.org/cd/transc1#sin": "sin",
 		"http://www.openmath.org/cd/transc1#cos": "cos",
 		"http://www.openmath.org/cd/transc1#tan": "tan",
