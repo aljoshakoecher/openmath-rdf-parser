d10383bf03dec630c51aab6ba6f3120bba2c2f3c
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2023-08-25 10:16:10 +0200

    inital test implementation of fromOpenMath

--- src/parser.ts
+++ src/parser.ts
@@ -1,19 +1,67 @@
-import { parse } from "mathjs";
+import { parse as mathParse } from "mathjs";
 import { Namespace, graph, blankNode, lit, serialize } from "rdflib";
 import { OperatorDictionary } from "./dictionary";
 import { ArgumentListStore } from "./argumentListStore";
 
-export function fromOpenMath(openMathRdf: string) {
-	console.log(openMathRdf);
+import { QueryEngine } from "@comunica/query-sparql";
+import { Store, Parser as N3Parser } from "n3";
+
+const queryEngine = new QueryEngine();
+
+/**
+ * Converts an OpenMath RDF representation into a textual, human-readable formula
+ * @param openMathRdf A string in RDF serialization containing OpenMath
+ */
+export async function fromOpenMath(openMathRdf: string) {
+	// This function is implemente with an N3 store and Comunica's query functionality as rdflib just didn't implement the needed features
+	// Get a new store, create a parser and parse the input using a callback
+	const store = new Store();
+	const parser = new N3Parser();
+	parser.parse(openMathRdf, (error, quad, prefixes) => {
+		if (error) console.log(`Error while parsing`);
+		if (quad) store.add(quad);
+		else console.log("Done parsing!", prefixes);
+	});
+
+
+	// Query to get OpenMath applications with operators and variables. Works also for nested applications. Positions stores arguments position, 
+	// so that x+y and y+x can be distinguished. Protect this query at all cost...
+	const queryString = `
+	PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+	PREFIX OM: <http://openmath.org/vocab/math#>
+
+	SELECT ?application (count(?argumentList)-1 as ?position) ?operator ?argType ?argName WHERE {
+		?application OM:arguments/rdf:rest* ?argumentList;
+			OM:operator ?operator.
+		
+		?argumentList rdf:rest*/rdf:first ?arg.
+		?arg a ?argType.
+		OPTIONAL {
+			?arg OM:name ?argName.
+		}
+	}
+	GROUP BY ?application ?argName ?operator ?argType
+	`;
+	
+	// Fire query, get results (from a stream) and print all values for application for testing purposes
+	const bindingsStream = await queryEngine.queryBindings(queryString, {sources: [store]});
+	const bindings = await bindingsStream.toArray();
+	
+	console.log("results");
+	bindings.forEach(binding => console.log(binding.get('application').value));
+	
 }
 
 
 export function toOpenMath(formula: string) {
+	// This function is implemented rdflib.js as it is just easier to use when creating RDF (compared to Comunica)
+	// Define needed namespaces
 	const RDF = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');	// rdf NS
 	const OM_MATH = Namespace("http://openmath.org/vocab/math#");			// OpenMath Math NS. Basic concepts, e.g., Application, Variable
 	const XSD = Namespace("http://www.w3.org/2001/XMLSchema#");
 	const EX = Namespace("http://example.org/ontology#");					// Example NS. //TODO: Change to something useful later
 
+	// Define some shorthands for needed properties and classes
 	const rdfType = RDF('type');
 	const omApplication = OM_MATH('Application');
 	const omArguments = OM_MATH('arguments');
@@ -23,14 +71,10 @@ export function toOpenMath(formula: string) {
 	const omLiteral = OM_MATH('Literal');
 	const omLiteralValue = OM_MATH('value');
 
-	const store = graph();
-
-	const node = parse(formula);
-	// const baseApplication = EX('myApplication');
-	// store.add(baseApplication, rdfType, omApplication);
-	// const currentApplication = baseApplication;
+	// Parse the textual formula into a math.js representation
+	const node = mathParse(formula);
 	
-	console.log("traverse");
+	const store = graph();
 	const listStore = new ArgumentListStore();
 
 	node.traverse(function (node, path, parent) {
@@ -38,20 +82,13 @@ export function toOpenMath(formula: string) {
 		console.log(path);
 		console.log(parent);
 		
-		// if(parent == null) return;
-
-		// console.log(`Type: ${node.type}. Path: ${path}. Parent: ${parent}. Parent(args): ${node[path]}`);   
-		
-		// console.log(currentArgumentList);
 		
 		if( (node.type == 'FunctionNode')) {
 			const castNode = node as math.FunctionNode;
 			console.log(`Type: ${castNode.type}. Op/Name: ${castNode.fn}. Path: ${path}. Parent: ${parent}`);   
 			
-			// console.log({mathJsOperation: mathJsOperation});
 			const functionName = castNode.fn.name;
 			const operator = OperatorDictionary.getOpenMathSymbol(functionName);
-			// console.log({operator: operator});
 			
 			const application = EX(`myApplication_${functionName}`);
 			store.add(application, rdfType, omApplication);
@@ -68,9 +105,7 @@ export function toOpenMath(formula: string) {
 			const mathJsOperation = (node as math.OperatorNode)?.op ?? (node as math.SymbolNode)?.name;
 			console.log(`Type: ${node.type}. Op/Name: ${mathJsOperation}. Path: ${path}. Parent: ${parent}`);   
 			
-			// console.log({mathJsOperation: mathJsOperation});
 			const operator = OperatorDictionary.getOpenMathSymbol(mathJsOperation);
-			// console.log({operator: operator});
 			
 			const operation = blankNode();
 			store.add(operation, rdfType, omApplication);
@@ -112,24 +147,9 @@ export function toOpenMath(formula: string) {
 			const currentArgumentList = listStore.getCurrentArgumentList(parent);
 			currentArgumentList.append(constantNode);
 		}
-		
-		// switch (node.type) {
-		// case 'OperatorNode' || 'SymbolNode': {
-		// 	console.log(`Type: ${node.type}. Op: ${(node as math.OperatorNode).op}. Path: ${path}`);    
-		// 	const operatorNode = node as math.OperatorNode;
-		// 	const operator = OperatorDictionary.getOpenMathSymbol(operatorNode.op);
-		// 	store.add(currentApplication, omOperator, operator);
-
-		// 	const applicationArguments = new Collection();
-		// 	store.add(currentApplication, omArguments, applicationArguments);
-		// 	break;
-		// }
-		// case 'SymbolNode':   console.log(`Type: ${node.type}. Name: ${(node as math.SymbolNode).name}. Path: ${path}`);  break;
-		// case 'ConstantNode': console.log(`Type: ${node.type}. Value: ${(node as math.ConstantNode).value}. Path: ${path}`); break;
-		// default:             console.log(node.type);
-		// }
 	});
 
+	// Just print out the store's content for testing purposes
 	console.log(serialize(undefined, store, 'http://base.org/', 'text/turtle'));
 }
 
---
+++ src/test.ts
@@ -0,0 +1,24 @@
+import {fromOpenMath, toOpenMath} from "./index";
+const input = "sin(x+y)";
+toOpenMath(input);
+
+const ttlInput = ` 
+@prefix : </#>.
+@prefix ont: <http://example.org/ontology#>.
+@prefix m: <http://openmath.org/vocab/math#>.
+
+ont:myApplication_sin
+    a m:Application;
+    m:arguments
+            (
+                    [
+                        a m:Application;
+                        m:arguments
+                                ( [ a m:Variable; m:name "x" ]
+                                [ a m:Variable; m:name "y" ] );
+                        m:operator "http://www.openmath.org/cd/arith1#plus"
+                    ]
+            );
+    m:operator "http://www.openmath.org/cd/transc1#sin".`;
+
+fromOpenMath(ttlInput);
