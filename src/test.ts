2b8dfcd46b1a1e56977f02c920ab5d43dcd8a555
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2023-09-08 15:41:32 +0200

    added another test case

--- src/test.ts
+++ src/test.ts
@@ -21,4 +21,30 @@ ont:myApplication_sin
             );
     m:operator "http://www.openmath.org/cd/transc1#sin".`;
 
-fromOpenMath(ttlInput);
+const ttlInput2 = ` 
+@prefix : </#>.
+@prefix ont: <http://example.org/ontology#>.
+@prefix m: <http://openmath.org/vocab/math#>.
+
+ont:myApplication_sinpow
+    a m:Application;
+    m:arguments
+            (
+                    [
+                        a m:Application;
+                        m:arguments
+                                (
+									[
+										a m:Application;
+										m:arguments
+												( [ a m:Variable; m:name "x" ]
+												[ a m:Variable; m:name "y" ] );
+										m:operator "http://www.openmath.org/cd/arith1#plus"
+									]
+								);
+                        m:operator "http://www.openmath.org/cd/arith1#power"
+                    ]
+            );
+m:operator "http://www.openmath.org/cd/transc1#sin".`;
+
+fromOpenMath(ttlInput2);
