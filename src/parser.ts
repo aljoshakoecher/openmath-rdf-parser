e5c35d4dff7bcedd11d58b8d8780776c96678913
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2024-05-24 14:49:09 +0200

    restructuring the fromOpenMath function

--- src/parser.ts
+++ src/parser.ts
@@ -1,31 +1,51 @@
 import { parse as mathParse } from "mathjs";
 import { Namespace, graph, blankNode, lit, serialize } from "rdflib";
 import { MathJsSymbolInformation, OperatorDictionary } from "./dictionary";
-import { ArgumentListStore } from "./argumentListStore";
+import { ArgumentListMap } from "./argumentListStore";
 
 import { QueryEngine } from "@comunica/query-sparql";
 import { Bindings } from "@rdfjs/types";
-import { Store, Parser as N3Parser } from "n3";
+import { Store, Parser as N3Parser, } from "n3";
 
 const queryEngine = new QueryEngine();
 
 /**
- * Converts an OpenMath RDF representation into a textual, human-readable formula
- * @param openMathRdf A string in RDF serialization containing OpenMath
+ * Util function to convert the parsing function into a promise based one
+ * @param rdfString RDF string to parse into an N3 store
+ * @returns Promise of a store with the RDF string loaded into it
  */
-export async function fromOpenMath(openMathRdf: string) {
-	// This function is implemented with an N3 store and Comunica's query functionality as rdflib just didn't implement the needed features
-	// Get a new store, create a parser and parse the input using a callback
+function parseAsPromise(rdfString: string): Promise<Store> {
 	const store = new Store();
 	const parser = new N3Parser();
-	parser.parse(openMathRdf, (error, quad, prefixes) => {
-		if (error) console.log(`Error while parsing`);
-		if (quad) store.add(quad);
-		else console.log("Done parsing!", prefixes);
+	return new Promise(function(resolve, reject) {
+		// Parsing is implemented with an N3 store and Comunica's query functionality as rdflib just didn't implement the needed features
+		// Get a new store, create a parser and parse the input using a callback
+		parser.parse(rdfString, (error, quad, prefixes) => {
+			if (error) {
+				console.log(`Error while parsing`);
+				reject(error);
+			}
+			
+			if (quad) store.add(quad);
+
+			else {
+				console.log("Done parsing!");
+				resolve(store);
+			}
+		});
 	});
+}
+
+/**
+ * Converts an OpenMath RDF representation into a textual, human-readable formula
+ * @param rdfString A string in RDF serialization containing OpenMath
+ */
+export async function fromOpenMath(rdfString: string, rootApplicationIri: string): Promise<string> {
+	
+	const store = await parseAsPromise(rdfString);
 
 	// Query to get OpenMath applications with operators and variables. Works also for nested applications. Positions stores arguments position, 
-	// so that x+y and y+x can be distinguished. Protect this query at all cost...
+	// so that, e.g.,  "x / y" and "y / x" can be distinguished. Protect this query at all cost...
 	const queryString = `
 	PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 	PREFIX OM: <http://openmath.org/vocab/math#>
@@ -36,6 +56,7 @@ export async function fromOpenMath(openMathRdf: string) {
 
 		?argumentList rdf:rest*/rdf:first ?arg.
 		?arg a ?argType.
+		# ?argType rdfs:subClassOf OM:Object.
 		OPTIONAL {
 			?arg OM:name ?argName.
 		}
@@ -43,60 +64,92 @@ export async function fromOpenMath(openMathRdf: string) {
 	GROUP BY ?application ?argName ?operator ?argType ?arg
 	`;
 	
-	// Fire query, get results (from a stream) and print all values for application for testing purposes
+	// Fire query and get results as an array
 	const bindingsStream = await queryEngine.queryBindings(queryString, {sources: [store]});
 	const bindings = await bindingsStream.toArray();
 	
+	// Get root application to start the whole recursive parsing procedure
+	const rootApplication = getRootApplication(bindings, rootApplicationIri);
+	const string = getArgumentsOfApplication(rootApplication, bindings);
 	
-	const rootApplication = getRootApplication(bindings);
-	console.log(rootApplication.get("application"));
-
-	const openMathOperator = rootApplication.get("operator").value;
-	const operator = OperatorDictionary.getMathJsSymbol(openMathOperator);
-
-	const argumentExpression = getArgumentsOfApplication(rootApplication, bindings);
-	const string = createExpression(operator, argumentExpression);
-	console.log(string);
+	return string;
 }
 
+/**
+ * Creates a string expression for a given operator and arguments. Handles unary and binary functions 
+ * @param operator MathJsSymbolInformation of an operator containing operator symbol and arity
+ * @param argumentExpression Can be either a single argument (for unary operators) or multiple arguments in an array (for binary /n-ary)
+ * @returns 
+ */
 function createExpression(operator:MathJsSymbolInformation, argumentExpression: string[] |string) {
 	const arity = operator.arity;
 	const operatorSymbol = operator.symbol;
 	let expression = "";
 	if (arity == 1) {
+		// Unary operators are constructed like <operator>(argument), e.g., sin(x)
 		expression = `${operator.symbol}(${argumentExpression})`;
 	}
 	if (arity == 2) {
+		// Binary operators are constructed by concatenating operator and arguments, e.g. x + y + z...
 		expression = (argumentExpression as string[]).join(operatorSymbol);
 	}
 	return expression;
 }
 
 
-function getRootApplication(bindings: Bindings[]): Bindings {
-	// Get parent application (i.e. entry with a value for ?application that is no argument (?arg) to another parent application)
-	return bindings.find(binding => !bindings.some(b => b.get("arg") == binding.get("application")));
+/**
+ * Finds the root application element by searching for rootApplicationIri and making sure it is in fact a root application element.
+ * @param bindings All results of the parsing query
+ * @param rootApplicationIri IRI of the root application to search for
+ * @returns The root application entry within the list of all Bindings
+ */
+function getRootApplication(bindings: Bindings[], rootApplicationIri: string): Bindings {
+	// Due to the query structure, finding the parent element is a bit tricky. To understand this function, it's best to execute the query separately and look at the results 
+	// First, we filter for the given rootApplicationIri to look only for entries of the OpenMath expression we are interested in (this filters out other possible roots)
+	// In addition, we check that these candidates are in fact root applications. Check that the value for ?application is no argument (?arg) to a "higher" parent application
+	const rootCandidates = bindings.filter(binding => {
+		const matchesRootApplicationIri = binding.get("application").value === rootApplicationIri;
+		const hasNoHigherParent = !bindings.some(b => b.get("arg") == binding.get("application"));
+		return (matchesRootApplicationIri && hasNoHigherParent);
+	});
+
+	// We can then still have multiple rows within the bindings if the root application is a binary relation (e.g. for "y=x+z" and x=y).
+	// If there are no sub applications, any line can be returend. If there is a sub application, this one must be returned
+	const parentWithSubApplication = rootCandidates.find(rootCandidate => (rootCandidate.get("argType").value == "http://openmath.org/vocab/math#Application"));
+
+	let rootApplication;
+	if (parentWithSubApplication) {
+		// If there is a parent with a sub application, return this one
+		rootApplication = parentWithSubApplication;
+	} else {
+		// If not, simply return the first one. It doesn't matter as in later stages, all information will be retrieved
+		rootApplication =  rootCandidates[0];
+	}
+	
+	// if (!rootApplication) throw new Error(`Error while finding root application with IRI ${rootApplicationIri}`);
+
+	return rootApplication;
 }
 
 function getArgumentsOfApplication(parentApplication: Bindings, bindings: Bindings[]): string {
-	// Take the arg of the parentApplication and look it up in the list of arguments
-	const argumentEntries = bindings.filter(binding => binding.get("application").equals(parentApplication.get("arg")));
-	console.log(argumentEntries);
+	// Check if there are more entries with arguments under the current element's application. This is the case for non-nested terms like x+y+z...
+	const argumentEntries = bindings.filter(binding => binding.get("application").equals(parentApplication.get("application")));
+	
+	// Check if the entry has children. If it has, we need to recursively go deeper
+	const childApplications = bindings.filter(binding => binding.get("application").equals(parentApplication.get("arg")));
 	let string = "";
-	if (hasChildArguments(argumentEntries, bindings)) {
-		console.log("has child arguments");
-		const entry = argumentEntries[0];
-		console.log(entry.get("application").value);
-		string = getArgumentsOfApplication(entry, bindings);
-		console.log("part string");
-		console.log(string);
-		
-		
+
+	if (childApplications.length > 0) {
+		const entry = childApplications[0];
+		const openMathOperator = argumentEntries[0].get("operator").value;
+		const operator = OperatorDictionary.getMathJsSymbol(openMathOperator);
+		const argumentNames = argumentEntries.flatMap(entry => (entry.get("argName") !== undefined) ? entry.get("argName").value : []);
+		string = createExpression(operator, [...argumentNames, getArgumentsOfApplication(entry, bindings)]);
 	} else{
 		const allSameOperator = argumentEntries.every(entry => entry.get("operator").equals(argumentEntries[0].get("operator")));
 		if (!allSameOperator) {
 			const operators = argumentEntries.map(entry => entry.get("operator"));
-			throw new Error(`Error trying to obtain the operator of application. Operators found: ${operators.toString()}`);
+			throw new Error(`Error trying to obtain the operator of application. Multiple operators found: ${operators.toString()}`);
 		}
 		const openMathOperator = argumentEntries[0].get("operator").value;
 		const operator = OperatorDictionary.getMathJsSymbol(openMathOperator);
@@ -107,26 +160,15 @@ function getArgumentsOfApplication(parentApplication: Bindings, bindings: Bindin
 	return string;
 }
 
-function hasChildArguments(argumentEntries: Bindings[], allBindings: Bindings[]) {
-	const noOfEntries = argumentEntries.length;
-	// If argumentEntries has further children, then the array can contain only one entry
-	// TODO: Check if this is true
-	console.log("no of entries for" + argumentEntries[0].get("application").value);
-	console.log(noOfEntries);
-	
-	if (noOfEntries > 1) return false;
-
-	// Otherwise, if there is only one, this one has to point to other entries
-	const childElement = argumentEntries.find(argumentEntry => allBindings.some(binding => binding.get("application").equals(argumentEntry.get("arg"))));
-	return (noOfEntries == 1) && (childElement != undefined);
-}
 
 
 
-
-
-
-export function toOpenMath(formula: string) {
+/**
+ * Convert a mathematical expression from a string to an OpenMath RDF object
+ * @param formula A mathematical formula represented in string syntax
+ * @returns 
+ */
+export function toOpenMath(formula: string): string {
 	// This function is implemented rdflib.js as it is just easier to use when creating RDF (compared to Comunica)
 	// Define needed namespaces
 	const RDF = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');	// rdf NS
@@ -148,7 +190,7 @@ export function toOpenMath(formula: string) {
 	const node = mathParse(formula);
 	
 	const store = graph();
-	const listStore = new ArgumentListStore();
+	const listStore = new ArgumentListMap();
 
 	node.traverse(function (node, path, parent) {
 		console.log(node.type);
@@ -223,6 +265,6 @@ export function toOpenMath(formula: string) {
 	});
 
 	// Just print out the store's content for testing purposes
-	console.log(serialize(undefined, store, 'http://base.org/', 'text/turtle'));
+	return serialize(undefined, store, 'http://base.org/', 'text/turtle');
 }
 
