bf48a33daabc03ca56a3471969f6d0aae7acf0b5
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2024-06-11 15:15:27 +0200

    adds a dictionary to keep track of IRI / variable replacements

---
+++ src/IriVariableMap.ts
@@ -0,0 +1,59 @@
+
+/**
+ * A map utility to replace IRIs with variable names that OpenMath can understand
+ */
+export class IriVariableDictionary {
+	static base = 'omrdf';
+	static counter = 0;
+
+	static iriToVariable = new Map<string, string>();
+	static variableToIri = new Map<string, string>();
+	
+	/**
+	 * Creates a new mapping for an IRI
+	 * @param iri IRI that needs to be resolved
+	 * @returns The variable that replaces the IRI
+	 */
+	static addNewIri(iri: string): string {
+		const newVar = `${this.base}_${this.counter}`;
+		this.iriToVariable.set(iri, newVar);
+		this.variableToIri.set(newVar, iri);
+		this.counter++;
+		return newVar;
+	}
+
+	/**
+	 * Checks whether a given variable is contained in the dictionary
+	 * @param variable variable to check
+	 * @returns true, if variable is contained in the dictionary
+	 */
+	static containsVariable(variable: string): boolean {
+		return this.variableToIri.has(variable);
+	}
+
+	/**
+	 * Get a variable for an IRI to replace it in OpenMath expressions
+	 * @param iri IRI that is replaced by an OpenMath variable
+	 * @returns The valid OpenMath variable name
+	 */
+	static getVariableForIri(iri: string) {
+		return this.iriToVariable.get(iri);
+	}
+
+	/**
+	 * Get an IRI for a variable to replace it after OpenMath has been parsed
+	 * @param variable A valid OpenMath variable to be used in parsing
+	 * @returns An IRI that stands for the variable
+	 */
+	static getIriForVariable(variable: string) {
+		return this.variableToIri.get(variable);
+	}
+
+
+	static reset() {
+		this.variableToIri.clear();
+		this.iriToVariable.clear();
+		this.counter = 0;
+	}
+
+}
